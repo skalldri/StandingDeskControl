01d654e24a20b6df8e2d50184da1963b4858840f
Stuart Alldritt <s.k.alldritt@gmail.com>
2019-10-24 16:27:36 -0700

    Latest on HDD

--- StandingDeskControl/StartupTask.cs
+++ StandingDeskControl/StartupTask.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
